868dce93b1831eeaaa922a49b41051d932dba801
Patbodin Intajak <zpatbodin.i@pttdigital.com>
2022-08-14 19:39:23 +0700

    Add function to validate input args for checkLottery

--- checkLottery.js
+++ checkLottery.js
@@ -3,30 +3,85 @@ const utils = require('./utils');
 const chalk = require('chalk');
 const Enumberable = require('linq');
 
+function validateInputNumber(myInput) {
+    var result = true;
+
+    // console.log("input length => " + [2, 3, 6].includes(myInput.length));
+    if ([2, 3, 6].includes(myInput.length)){
+        var regex = /\d{x}/;
+        result = new RegExp(regex.source.replace("x", myInput.length), 'g').test(myInput);
+    } 
+    else {
+        result = false;
+    }
+
+    return result;
+}
+
+function validateFileName(myInput) {
+    var result = true;
+
+    // console.log("file name => " + myInput);
+    var regex = /\d{1,2}_\d{1,2}_\d{4}/;
+    result = new RegExp(regex, 'g').test(myInput);
+
+    return result;
+}
+
 function main(){
     // console.log(chalk.red("Hello Test"));
+    const myArgs = process.argv.slice(2);
+    console.log(chalk.green('myArgs: ', myArgs));
 
-    const objJSON = utils.findLotteryFile("1_10_2564");
-    // const test = objJSON.filter(x => x.two_suffix[0] == 83);
-    // console.log(objJSON);
-    // Enumberable.from(objJSON).where(x => x == "83").select(x => x).log().toJoinedString();
-
-    const aaa = RegExp(/83/g);
-
-    for (const key in objJSON) {
-        // console.log('key: ' + key);
-        // console.log('value: ' + objJSON[key] + ' --- ' + objJSON[key].length);
-        if(key != "name") {
-            // console.log('value: ' + objJSON[key] + ' --- ' + objJSON[key].length);
-            // var xx = Enumberable.from(objJSON[key]).where(x => x == "83").select(x => x).toJSONString();
-            var xx = Enumberable.from(objJSON[key]).where(x => x.match(aaa)).select(x => x).toJSONString();
-            console.log(xx);
-        }
+    if (myArgs.length == 0) {
+        // const objJSON = utils.findLotteryFile("1_10_2564");
+        // console.log(objJSON);
+        console.log(chalk.hex("#e3d37d")("Please input args!!!"));
     }
-    // console.log("test => " + objJSON['secondAward']);
+    else if (myArgs.length == 1) {
+        if(!validateInputNumber(myArgs[0])) {
+            console.log(chalk.hex("#a7327c")("Invalid args!!!"));
+            process.exit(1);
+        }
 
-    const bbb = /abcd/g;
-    console.log('====> ' + bbb.source.replace("abcd", "\d\d\d89"));
+        // const objJSON = utils.findLotteryFile("1_10_2564");
+        // console.log(objJSON);
+        console.log(chalk.hex("#b3d5fb")("Searching that number in all files..."));
+    }
+    else if (myArgs.length == 2) {
+        if(!validateInputNumber(myArgs[0]) || !validateFileName(myArgs[1])) {
+            console.log(chalk.hex("#a7327c")("Invalid args!!!"));
+            process.exit(1);
+        }
+
+        const objJSON = utils.findLotteryFile("1_10_2564");
+        // const test = objJSON.filter(x => x.two_suffix[0] == 83);
+        // console.log(objJSON);
+        // Enumberable.from(objJSON).where(x => x == "83").select(x => x).log().toJoinedString();
+
+        const aaa = RegExp(/83/g);
+
+        for (const key in objJSON) {
+            // console.log('key: ' + key);
+            // console.log('value: ' + objJSON[key] + ' --- ' + objJSON[key].length);
+            if(key != "name") {
+                console.log('key: ' + key);
+                // console.log('value: ' + objJSON[key] + ' --- ' + objJSON[key].length);
+                // var xx = Enumberable.from(objJSON[key]).where(x => x == "83").select(x => x).toJSONString();
+                var xx = Enumberable.from(objJSON[key]).where(x => x.match(aaa)).select(x => x).toJSONString();
+                console.log(xx);
+            }
+        }
+        // console.log("test => " + objJSON['secondAward']);
+
+        const bbb = /abcd/g;
+        console.log('====> ' + bbb.source.replace("abcd", "\d\d\d89"));
+    }
+    else {
+        console.log(chalk.hex("#a7327c")("Invalid args!!!"));
+    }
+
+    
 }
 
 main();
